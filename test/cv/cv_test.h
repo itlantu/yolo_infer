24c66726badc966b0c0323a3ef3d7ca0a2664775
lantu <it_lantu@outlook.com>
2024-09-03 20:01:37 +0800

    新增cv_test通用头文件

---
+++ test/cv/cv_test.h
@@ -0,0 +1,10 @@
+#ifndef YOLO_INFER_TEST_CV_TEST_H
+#define YOLO_INFER_TEST_CV_TEST_H
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+void sample_test();
+void resize_test();
+
+#endif //YOLO_INFER_TEST_CV_TEST_H
