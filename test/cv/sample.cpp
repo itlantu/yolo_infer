0bd1283de3753685ef46e0662af86662c548d24c
lantu <it_lantu@outlook.com>
2024-09-03 20:01:22 +0800

    导入cv_test.h头文件

--- test/cv/sample.cpp
+++ test/cv/sample.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
+#include "cv_test.h"
 
-int main(){
+void sample_test(){
     cv::Mat random_matrix(28, 28, CV_8UC3);
     cv::RNG rng;
     rng.fill(random_matrix, cv::RNG::UNIFORM, 0, 255);
 
     std::cout << "cv sample" << std::endl;
     std::cout << random_matrix << std::endl;
-
-    return 0;
 }
