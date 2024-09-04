9b8609ea592a7df8dfdb13ffd113cc09e3d01be4
lantu <it_lantu@outlook.com>
2024-09-04 17:58:54 +0800

    新增对ndim值的判断，如果值不正确则抛出一个runtime error

--- test/core/shape.cpp
+++ test/core/shape.cpp
@@ -10,7 +10,13 @@ void shape_test(){
     cout << shape1.to_string() << endl;
     cout << shape2.to_string() << endl;
 
-    cout << "ndim: " << shape1.ndim();
+    cout << "ndim: " << shape1.ndim() << endl;
+    if(shape1.ndim() != 4){
+        cout << "test/shape.cpp: ndim error, ndim !=4 = " << shape1.ndim() << endl;
+        throw runtime_error("test/shape.cpp: ndim error");
+    }
+
+
     for(int i = 0; i < 4; i++){
         if(shape1[i] != i + 1){
             cout << "test/shape.cpp: value error, i = " << i << endl;
