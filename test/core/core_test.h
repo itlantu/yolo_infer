038a4765cc73591fdeab3bde2ec064d6ba93c141
lantu <it_lantu@outlook.com>
2024-09-04 17:33:30 +0800

    导入shape.h头文件

---
+++ test/core/core_test.h
@@ -0,0 +1,10 @@
+#ifndef YOLO_INFER_TEST_CORE_TEST_H
+#define YOLO_INFER_TEST_CORE_TEST_H
+
+#include "infer/core/shape.h"
+#include <array>
+#include <iostream>
+
+void shape_test();
+
+#endif //YOLO_INFER_TEST_CORE_TEST_H
