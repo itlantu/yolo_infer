010c36914ae03f1cb38cf493f4931376bd78da69
lantu <it_lantu@outlook.com>
2024-09-05 10:44:54 +0800

    新增tensor_type_test函数对TensorType的基本功能进行测试

---
+++ test/core/tensor_type.cpp
@@ -0,0 +1,15 @@
+﻿#include "core_test.h"
+
+using namespace std;
+
+void tensor_type_test(){
+    infer::TensorType<float> t1;
+    infer::TensorType<int8_t> t2;
+    cout << "t1: " << t1.to_string() << endl;
+    cout << "t2: " << t2.to_string() << endl;
+
+    if((t1 == t2) || !(t1 != t2)){
+        cout << "tensor_type_test: equal error" << endl;
+        throw runtime_error("tensor_type_test: equal error");
+    }
+}
