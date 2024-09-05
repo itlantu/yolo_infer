2133f9be5f1d95a14ed2407be385574fc7fcb73b
lantu <it_lantu@outlook.com>
2024-09-05 10:44:12 +0800

    新增tensor_type_test函数声明

--- test/core/core_test.h
+++ test/core/core_test.h
@@ -2,9 +2,11 @@
 #define YOLO_INFER_TEST_CORE_TEST_H
 
 #include "infer/core/shape.h"
+#include "infer/core/tensor.h"
 #include <array>
 #include <iostream>
 
 void shape_test();
+void tensor_type_test();
 
 #endif //YOLO_INFER_TEST_CORE_TEST_H
