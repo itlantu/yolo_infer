283dc4c4f991ecd8d7d19f2d9478880e6a1c4f4c
lantu <it_lantu@outlook.com>
2024-09-10 15:15:26 +0800

    新增了utils_set_align_string，utils_add_test_code函数，新增了utils_add_test_file宏，修改了utils_test_project函数

--- cmake/Findutils.cmake
+++ cmake/Findutils.cmake
@@ -3,7 +3,9 @@ message(STATUS "cmake - os:         ${CMAKE_HOST_SYSTEM_NAME}")
 message(STATUS "cmake - compiler:   ${CMAKE_CXX_COMPILER_ID} (${CMAKE_CXX_COMPILER_VERSION})")
 message(STATUS "")
 
+set(test_id 0)
 set(is_MSVC OFF)
+
 if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "MSVC")
     set(is_MSVC ON)
 endif()
@@ -39,11 +41,75 @@ function(utils_target_link_src project)
     target_link_libraries(${project} SRC_LIBS)
 endfunction()
 
-# 传入测试项目名称和编号, 进行单元测试
-function(utils_test_project project id)
-    message(STATUS "")
-    message(STATUS "${id}) ${project} - test: running")
-    utils_target_link_src(${project})
-    add_test("test_${project}" ${project})
-    message(STATUS "${id}) ${project} - test: end")
+# 传入原始字符和对齐长度，生成左对齐
+function(utils_set_align_string output str num)
+    string(LENGTH "${str}" length)
+    set(space "")
+    # 插入空格
+    while(${num} GREATER ${length})
+        string(APPEND space " ")
+        math(EXPR length "${length} + 1")
+        set(length  ${length} PARENT_SCOPE)
+        set(space   ${space} PARENT_SCOPE)
+    endwhile()
+    set(${output} "${str}${space}" PARENT_SCOPE)
 endfunction()
+
+# 传入测试项目名称, 进行单元测试
+function(utils_test_project _project)
+    math(EXPR id "${test_id} + 1")
+    set(test_id ${id} PARENT_SCOPE)
+
+    utils_set_align_string(test_name    "${_project}" 25)
+
+    message(STATUS "test${id}: \t ${test_name} - build")
+    add_test("test_${_project}" ${_project})
+endfunction()
+
+# 传入测试项目名称, 测试文件路径, 库名称, 生成对应名称的project并且根据文件路径生成可执行文件，再链接库
+# example:
+#   set(links "opencv" "onnxruntime")
+#   _utils_add_test_code _project("cv_resize" ${CMAKE_CURRENT_SOURCE_DIR}/cv/resize.cpp} ${links})
+function(utils_add_test_code _project _path _link)
+    project("${_project}")
+    add_executable(${_project} ${_path})
+
+    foreach(library ${_link})
+        if("${_link}" STREQUAL "opencv")
+            utils_target_link_opencv("${_project}")
+        elseif("${_link}" STREQUAL "onnxruntime")
+            utils_target_link_onnxruntime("${_project}")
+        elseif("${_link}" STREQUAL "src")
+            utils_target_link_src("${_project}")
+        else()
+            message(FATAL_ERROR "unknown link library: ${library}")
+        endif()
+    endforeach()
+    set(test_id ${test_id})
+    utils_test_project("${_project}")
+    set(test_id ${test_id} PARENT_SCOPE)
+endfunction()
+
+# 传入测试项目名称, 测试文件路径, LINK, 库名称， 辅助调用_utils_add_test_code函数
+# example:
+#   utils_add_test_file("${CMAKE_CURRENT_SOURCE_DIR}/cv/resize.cpp}" LINK OpenCV)
+macro(utils_add_test_file _path)
+    set(_link)
+    # 得到_project
+    # 去掉.../test/
+    string(REGEX REPLACE "[A-Za-z_:/\\]*test/" "" _project ${_path})
+    # 去掉.cpp
+    string(REGEX REPLACE ".cpp" "" _project ${_project})
+    # 替换'/'为'_'
+    string(REGEX REPLACE "/" "_" _project ${_project})
+
+    foreach (library ${ARGN})
+        # 转小写
+        string(TOLOWER "${library}" library)
+        if("${library}" STREQUAL "link")
+            continue()
+        endif()
+        list(APPEND _link "${library}")
+    endforeach()
+    utils_add_test_code(${_project} ${_path} ${_link})
+endmacro()
