64c2b0dc2ba49b57b556ff07fbabf348143bf74e
lantu <it_lantu@outlook.com>
2024-09-11 17:07:56 +0800

    新增onnx_test.h头文件

---
+++ test/onnx/onnx_test.h
@@ -0,0 +1,9 @@
+#ifndef YOLO_INFER_TEST_ONNX_H
+#define YOLO_INFER_TEST_ONNX_H
+
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <onnxruntime_cxx_api.h>
+
+#endif //YOLO_INFER_TEST_ONNX_H
